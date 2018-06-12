b90160ce1bf8a1125a7148cd1ad47e3260606f32
Felipe Ramos <felipecramos@ufrn.edu.br>
2018-06-12 08:27:03 -0300

    Separated hpp

--- include/hashtable.hpp
+++ include/hashtable.hpp
@@ -8,6 +8,9 @@
 #include <math.h>
 #include <string>
 
+/*! \file 	hash.hpp
+ * 	\author	Felipe Ramos */
+
 using size_type = size_t;		//!< Simple Alias
 
 template <
@@ -45,115 +48,36 @@ class HashTbl{
 
 		/** Inserts Elements related to a `key_` on the table.
 		 *	\return True if the insertion was successful, false otherwise. */ 
-		bool insert( const KeyType & key_, const DataType & data_item_ ){
-		/*{{{*/
-			/* Elements list */	
-			auto & whichList = Lists[ hashFunc( key_ ) % tablesize ];
-			HashEntry new_entry( key_, data_item_ );
-			auto itr = whichList.begin(); 							//!< Iterator to the List's begin
-			auto itr_b = whichList.before_begin(); 					//!< Iterator to the position before the list's begin 
-			auto end = whichList.end(); 							//!< Iterator to the list's end
-			for ( /* */; itr != end; ++itr ){
-				itr_b++;
-				if ( equalFunc((*itr).m_key, new_entry.m_key) )
-					return false;
-			}
-
-			whichList.insert_after( itr_b, new_entry );
-
-			if( ++currentSize > tablesize ) 
-				rehash();
-
-			return true; 		// If operation was successful
-		}
-		/*}}}*/
+		bool insert( const KeyType &, const DataType & );
 
 		/** Recovers the information related to the `key_`.
 		 *	\return True if it finds the information, False otherwise. */
-		bool retrieve( const KeyType & key_,  DataType & data_item_ ) const{
-		/*{{{*/
-			/* Elements list */
-			auto & whichList = Lists[ hashFunc(key_) % tablesize ];
-			for( auto i = whichList.begin(); i != whichList.end(); ++i ){
-				if ( equalFunc((*i).m_key , key_) ){
-					data_item_ = (*i).m_data;
-					return true;
-				}
-			}
-			return false;
-
-		}
-		/*}}}*/
+		bool retrieve( const KeyType &,  DataType & ) const;
 
 		/** Prints the entire hash table. */
-		void print() const{
-		/*{{{*/
-			if( !Lists.empty() ){
-				for( auto Lists_iter = Lists.begin(); Lists_iter != Lists.end(); ++Lists_iter ){
-					if( !(*Lists_iter ).empty() )
-						for( auto j = (*Lists_iter).begin(); j != (*Lists_iter).end(); ++j ){
-							std::cout << "Key< ";
-							std::cout << (*j).m_key;
-							std::cout << " >, Data< ";
-							std::cout << (*j).m_data;
-							std::cout << " >\n";
-						}
-				}
-
-			}
-		}
-		/*}}}*/
+		void print() const;
 
 		/** Clear the lists related memories. */
-		void clear(){
-		/*{{{*/
-			Lists.clear();
-		}
-		/*}}}*/
+		void clear();
 		
 		/** Tells if a HashTbl is empty. */
-		bool empty() const{
-		/*{{{*/
-			return this->currentSize == 0;
-		}
-		/*}}}*/
+		bool empty() const;
 
 		/** Tells the current total elements are in a list. */
-		size_type count() const{
-		/*{{{*/
-			return this->currentSize;
-		}
-		/*}}}*/
+		size_type count() const;
 
 		/** Tells how many elements a table supports. */
-		size_type capacity() const{
-		/*{{{*/
-			return tablesize;
-		}
-		/*}}}*/
+		size_type capacity() const;
 
 		/** Removes an specific element `key_`. 
 		 * \return True if the `key_` was found, false otherwise */
-		bool remove( const KeyType & key_ ){
-		/*{{{*/
-			auto &whichList = Lists[ hashFunc(key_) % tablesize ];
-			auto itr_back = whichList.before_begin();
-			for( auto i = whichList.begin(); i != whichList.end(); ++i ){
-				if( equalFunc((*i).m_key, key_ ) ){
-					whichList.erase_after( itr_back );
-					--currentSize;
-					return true;
-				}
-				itr_back++;
-			}	
-			return false;
-		}
-		/*}}}*/
+		bool remove( const KeyType & );
 	/*}}}*/
 
 	private:
 	/*{{{*/
-		std::vector<std::forward_list<HashEntry>> Lists; //!< vector with List of elements (collisions)
+		/* Vector with the list of elements (for collisions) */
+		std::vector< std::forward_list<HashEntry> > Lists;
 		
 		size_type currentSize; 		//!< Total of elements in the table
 		size_type tablesize; 		//!< Hash table current size
@@ -163,36 +87,156 @@ class HashTbl{
 		KeyEqual equalFunc;
 
 		/** Helps finding the next prime number after table size. */
-		size_type next_prime(size_type number){
-		/*{{{*/
-			for( size_type i = 2; i < sqrt( number ); ++i ) {
-				if(number % i == 0) return next_prime( number + 1 );
-			}
-			return number;
-		}
-		/*}}}*/
+		size_type next_prime( size_type );
 		
 		/** Resizes the Hash Table. */
-		void rehash(){
-		/*{{{*/
-			std::vector< std::forward_list<HashEntry> > oldLists = Lists;
-			
-			/* Creates a new double-sized empty table */
-			tablesize = next_prime( tablesize * 2 );
-			
-			Lists.resize( tablesize );
-			currentSize = 0;
-		
-			for( auto & thisList : Lists )
-				thisList.clear(); 
-
-			/* Copies table over */
-			for( auto & copyLists : oldLists )
-				for( auto & x : copyLists )
-					insert( x.m_key, x.m_data );
-		}	
-		/*}}}*/
+		void rehash();
 	/*}}}*/
 };
+
+// Source Implementation
+
+/* Public Implement {{{*/
+template < class KeyType, class DataType, class KeyHash, class KeyEqual	>
+bool HashTbl<KeyType, DataType, KeyHash, KeyEqual>::insert
+( const KeyType &key_, const DataType &data_item_ ){
+/*{{{*/
+	/* Elements list */	
+	auto & whichList = Lists[ hashFunc( key_ ) % tablesize ];
+	HashEntry new_entry( key_, data_item_ );
+	auto itr = whichList.begin(); 							//!< Iterator to the List's begin
+	auto itr_b = whichList.before_begin(); 					//!< Iterator to the position before the list's begin 
+	auto end = whichList.end(); 							//!< Iterator to the list's end
+	for ( /* */; itr != end; ++itr ){
+		itr_b++;
+		if ( equalFunc((*itr).m_key, new_entry.m_key) )
+			return false;
+	}
+
+	whichList.insert_after( itr_b, new_entry );
+
+	if( ++currentSize > tablesize ) 
+		rehash();
+
+	return true; 		// If operation was successful
+}
+/*}}}*/
+
+template < class KeyType, class DataType, class KeyHash, class KeyEqual	>
+bool HashTbl<KeyType, DataType, KeyHash, KeyEqual>::retrieve
+( const KeyType &key_, DataType &data_item_ ) const{
+/*{{{*/
+	/* Elements list */
+	auto & whichList = Lists[ hashFunc(key_) % tablesize ];
+	for( auto i = whichList.begin(); i != whichList.end(); ++i ){
+		if ( equalFunc((*i).m_key , key_) ){
+			data_item_ = (*i).m_data;
+			return true;
+		}
+	}
+	return false;
+}
+/*}}}*/
+
+template < class KeyType, class DataType, class KeyHash, class KeyEqual	>
+void HashTbl<KeyType, DataType, KeyHash, KeyEqual>::print( void ) const{
+/*{{{*/
+	if( !Lists.empty() ){
+		for( auto Lists_iter = Lists.begin(); Lists_iter != Lists.end(); ++Lists_iter ){
+			if( !(*Lists_iter ).empty() )
+				for( auto j = (*Lists_iter).begin(); j != (*Lists_iter).end(); ++j ){
+					std::cout << "Key< ";
+					std::cout << (*j).m_key;
+					std::cout << " >, Data< ";
+					std::cout << (*j).m_data;
+					std::cout << " >\n";
+				}
+		}
+
+	}
+}
+/*}}}*/
+
+template < class KeyType, class DataType, class KeyHash, class KeyEqual	>
+void HashTbl<KeyType, DataType, KeyHash, KeyEqual>::clear( void ){
+/*{{{*/
+	Lists.clear();
+}
+/*}}}*/
+
+template < class KeyType, class DataType, class KeyHash, class KeyEqual	>
+bool HashTbl<KeyType, DataType, KeyHash, KeyEqual>::empty( void ) const{
+/*{{{*/
+	return this->currentSize == 0;
+}
+/*}}}*/
+
+template < class KeyType, class DataType, class KeyHash, class KeyEqual	>
+size_type HashTbl<KeyType, DataType, KeyHash, KeyEqual>::count( void ) const{
+/*{{{*/
+	return this->currentSize;
+}
+/*}}}*/
+
+template < class KeyType, class DataType, class KeyHash, class KeyEqual	>
+size_type HashTbl<KeyType, DataType, KeyHash, KeyEqual>::capacity( void ) const{
+/*{{{*/
+	return this->tablesize;
+}
+/*}}}*/
+
+template < class KeyType, class DataType, class KeyHash, class KeyEqual	>
+bool HashTbl<KeyType, DataType, KeyHash, KeyEqual>::remove( const KeyType &key_ ){
+/*{{{*/
+	auto &whichList = Lists[ hashFunc(key_) % tablesize ];
+	auto itr_back = whichList.before_begin();
+	for( auto i = whichList.begin(); i != whichList.end(); ++i ){
+		if( equalFunc((*i).m_key, key_ ) ){
+			whichList.erase_after( itr_back );
+			--currentSize;
+			return true;
+		}
+		itr_back++;
+	}	
+	return false;
+}
+/*}}}*/
+
+/*}}}*/
+
+/* Private Implement {{{*/
+
+template < class KeyType, class DataType, class KeyHash, class KeyEqual	>
+size_type HashTbl<KeyType, DataType, KeyHash, KeyEqual>::next_prime( size_type number ){
+/*{{{*/
+	for( size_type i = 2; i < sqrt( number ); ++i ) {
+		if(number % i == 0) return next_prime( number + 1 );
+	}
+	return number;
+}
+/*}}}*/
+
+template < class KeyType, class DataType, class KeyHash, class KeyEqual	>
+void HashTbl<KeyType, DataType, KeyHash, KeyEqual>::rehash( void ){
+/*{{{*/
+	std::vector< std::forward_list<HashEntry> > oldLists = Lists;
+	
+	/* Creates a new double-sized empty table */
+	tablesize = next_prime( tablesize * 2 );
+	
+	Lists.resize( tablesize );
+	currentSize = 0;
+
+	for( auto & thisList : Lists )
+		thisList.clear(); 
+
+	/* Copies table over */
+	for( auto & copyLists : oldLists )
+		for( auto & x : copyLists )
+			insert( x.m_key, x.m_data );
+}
+/*}}}*/
+
+/*}}}*/
 
 #endif
